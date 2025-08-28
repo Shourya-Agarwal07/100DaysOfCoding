419e258aef18c73acb0aba597e84c8b296fc2304
Shourya-Agarwal07 <shouryaa03@gmail.com>
2025-08-28 13:20:45 +0530

    Operations

---
+++ 02_program.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int sum;
+    int difference;
+    int product;
+    int division;
+    printf("Enter your first number");
+    scanf("%d", &a);
+    printf("Your first number is %d\n", a);
+    printf("Enter your second number");
+    scanf("%d", &b);
+    printf("Your second number is %d\n", b);
+    printf("Sum is %d\n", sum = a + b);
+    printf("Difference is %d\n", difference = a - b);
+    printf("product is %d\n", product = a * b);
+    printf("Quotient is %d", division = a / b);
+
+    return 0;
+}
